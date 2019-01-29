79caf7979431bf168c507b797f4adedd43d606f7
Larzs <gatis@glaive.pro>
2019-01-30 00:29:45 +0200

    Created facade Invytr

---
+++ src/Facades/Invytr.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GlaivePro\Invytr\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Invytr extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'invytr';
+    }
+}
