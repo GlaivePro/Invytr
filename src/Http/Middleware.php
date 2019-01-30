e56ca8fe88157655a2247d3cf9ee010980e227aa
Larzs <gatis@glaive.pro>
2019-01-30 02:18:49 +0200

    added session check

--- src/Http/Middleware.php
+++ src/Http/Middleware.php
@@ -9,6 +9,13 @@ class Middleware
 {
     public function handle($request, Closure $next) 
     {
+        if($request->route() == 'password.update' && $request->session()->has('invytr')) {
+            if(config('auth.passwords.users.invites_expire')) {
+                config(['auth.passwords.users.expire' => config('auth.passwords.users.invites_expire')]);
+            }
+
+            $request->session()->forget('invytr');
+        }
         return $next($request);
     }
 }
