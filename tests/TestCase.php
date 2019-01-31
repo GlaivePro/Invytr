c2976e5379a164033a2f8a0201223d6bf8a2cf61
Larzs <gatis@glaive.pro>
2019-02-01 00:56:51 +0200

    Added test boilerplate

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+namespace GlaivePro\Invytr\Tests;
+//use lasselehtinen\MyPackage\MyPackageFacade;
+//use lasselehtinen\MyPackage\MyPackageServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return lasselehtinen\MyPackage\MyPackageServiceProvider
+     */
+    // protected function getPackageProviders($app)
+    // {
+    //     return [MyPackageServiceProvider::class];
+    // }
+    // *
+    //  * Load package alias
+    //  * @param  \Illuminate\Foundation\Application $app
+    //  * @return array
+     
+    // protected function getPackageAliases($app)
+    // {
+    //     return [
+    //         'MyPackage' => MyPackageFacade::class,
+    //     ];
+    // }
+}
