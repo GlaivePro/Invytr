363c24f45b6cc130cdc446b02724c317469c1b4b
Larzs <gatis@glaive.pro>
2019-02-03 00:19:52 +0200

    Added middleware unit test

--- tests/Unit/MiddlewareTest.php
+++ tests/Unit/MiddlewareTest.php
@@ -1,11 +1,18 @@
 <?php
 namespace GlaivePro\Invytr\Tests\Unit;
 
-//use MyPackage;
-
+use GlaivePro\Invytr\Http\Middleware;
 use GlaivePro\Invytr\Tests\TestCase;
 
 class MiddlewareTest extends TestCase
 {
+    public function testHandle()
+    {     
+        $request = new \Illuminate\Http\Request();
 
+        $middleware = new Middleware();        
+
+        $result = $middleware->handle($request, function(){ return 'works';});
+        $this->assertEquals('works', $result);
+    }
 }
