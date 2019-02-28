ad49f39e6cdddcd7feed0bbd3e198333478ecf28
Larzs <gatis@glaive.pro>
2019-02-28 17:06:43 +0200

    Update InvytrTest.php

--- tests/Feature/InvytrTest.php
+++ tests/Feature/InvytrTest.php
@@ -11,7 +11,7 @@ class InvytrTest extends TestCase
     /**
      * Setup the test environment.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
